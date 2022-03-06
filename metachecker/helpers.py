ce24fefd493d0139a628064c258ab6b579e17b90
lza_menace <lza_menace@protonmail.com>
2022-03-05 17:35:29 -0800

    update token cards

--- metachecker/helpers.py
+++ metachecker/helpers.py
@@ -1,3 +1,4 @@
+import ipfsApi
 from eth_account.messages import encode_defunct
 
 from web3.auto import w3
@@ -9,4 +10,32 @@ def verify_signature(message, signature, public_address):
     if recovered.lower() == public_address.lower():
         return True
     else:
+        return False
+
+def upload_to_ipfs(meme_id: str):
+    meme = Meme.query.get(meme_id)
+    if not meme:
+        return False
+    try:
+        client = ipfsApi.Client('127.0.0.1', 5001)
+        artwork_hashes = client.add(meme.get_fs_path())
+        artwork_hash = artwork_hashes[0]['Hash']
+        print(f'[+] Uploaded artwork to IPFS: {artwork_hash}')
+        meta = {
+            'name': meme.title,
+            'description': meme.description,
+            'image': f'ipfs://{artwork_hash}',
+            'create_date': meme.create_date.strftime('%Y-%m-%d %H:%M:%S'),
+            'file_name': meme.file_name,
+            'ipfs_hash': artwork_hash,
+            'properties': {
+                'creator_handle': meme.user.handle,
+                'creator_address': meme.user.public_address
+            }
+        }
+        meta_hash = client.add_json(meta)
+        print(f'[+] Uploaded metadata to IPFS: {meta_hash}')
+        return (meta_hash, artwork_hash)
+    except Exception as e:
+        print(f'[!] Error: {e}')
         return False
