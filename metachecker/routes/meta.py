af8814f24715c451980c9f30775eef8540671eff
lza_menace <lza_menace@protonmail.com>
2022-03-14 15:06:10 -0700

    tricky route to fetch assets from local ipfs server

--- metachecker/routes/meta.py
+++ metachecker/routes/meta.py
@@ -1,6 +1,9 @@
+import requests
 from flask import Blueprint, render_template
 from flask import redirect, url_for
 from flask_login import logout_user
+
+from metachecker import config
 
 
 bp = Blueprint('meta', 'meta')
@@ -15,3 +18,9 @@ def about():
 def disconnect():
     logout_user()
     return redirect(url_for('collection.index'))
+
+@bp.route('/ipfs/<path:path>')
+def load_ipfs(path):
+    ipfs_uri = f'{config.IPFS_SERVER}/ipfs/{path}'
+    res = requests.get(ipfs_uri, timeout=30)
+    return res.content
